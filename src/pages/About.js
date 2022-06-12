588bc3c03d77e32014383606f4624736843bc53e
Julie Webb <julief.webb@gmail.com>
2022-06-12 14:09:40 -0400

    achieves mvp

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AboutPage = () => {
-    return <h1>This is the About Page</h1>
+    return (
+        <div>
+            <h1>This is the About Page</h1>
+            <p>This is a simple app to practice using react routers</p>
+        </div>
+    );
 };
 
 export default AboutPage;
--- src/pages/Prices.js
+++ src/pages/Prices.js
@@ -10,16 +10,23 @@ const PricePage = (props) => {
     console.log(symbol);
 
     // use stock symbol to select from out array 
-    const stock = stocks.filter( ele => ele.symbol === symbol);
+    const stock = stocks.filter( ele => ele.symbol === symbol)[0];
     console.log(stock);
 
     // 
 
     return (
         <div>
-            <h1>{stock.name}</h1>
+            <h1>{stock.name} - {stock.symbol}</h1>
+            <h2>Last Price: {stock.lastPrice}</h2>
+            <h3>Change: {stock.change}</h3>
+            <h4>High: {stock.high}</h4>
+            <h4>Low: {stock.low}</h4>
+            <h4>Open: {stock.open}</h4>
+
         </div>
     );
 };
+
 
 export default PricePage;
